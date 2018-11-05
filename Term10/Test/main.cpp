0d1b6141dc8c9d80ae908978e3209bbf5b40ba6b
DeSerg <sergpopov95@gmail.com>
2018-11-05 04:42:47 +0300

    download refactoring

--- Term10/Test/main.cpp
+++ Term10/Test/main.cpp
@@ -1,6 +1,3 @@
-#include <cstdio>
-#include <memory.h>
-#include <cerrno>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -8,19 +5,27 @@
 #include <netdb.h>
 #include <unistd.h>
 
-#include <string>
 #include <iostream>
-#include <fstream>
-#include <vector>
+#include <iomanip>
 #include <sstream>
+#include <fstream>
+#include <string>
+#include <vector>
 
 
-constexpr int BufferSize = 16384;
+constexpr int Bytes2pow10 = 1024;
+
+constexpr int BufferSize = 4096;
 
 const char *ProtocolHttp = "http";
 
 const char *ProtocolPrefixHttp = "http://";
-const int ProtocolPrefixHttpLength = static_cast<int>(strlen(ProtocolPrefixHttp));
+const int ProtocolPrefixHttpLength = strlen(ProtocolPrefixHttp);
+
+const char *ProtocolPrefixHttps = "https://";
+
+const char *BodyDelimeter = "\r\n\r\n";
+const int BodyDelimiterLength = strlen(BodyDelimeter);
 
 
 class HttpFileDownloader {
@@ -28,46 +33,79 @@ class HttpFileDownloader {
 public:
 
     bool file_download(const std::string &url, const std::string &filename) {
-        int ipv,r,port;
-        std::string protocol, host_name, path, query, url_port;
-        std::vector<std::string> ip_addresses;
-
-        size_t pos2,pos3,pos4;
-
-        int offset = 0;
-        // detect protocol
-        if (url.rfind(ProtocolPrefixHttp, 0) == 0) {
-            offset = ProtocolPrefixHttpLength;
-            protocol = ProtocolHttp;
+        std::string host_name;
+        std::string path;
+        int port = 0;
+        if (!url_parse(url, host_name, path, port)) {
+            std::cout << "Failed to parse url" << std::endl;
+            return false;
         }
 
-        size_t slash_pos_1 = url.find_first_of('/', offset + 1);
+        std::vector<std::string> ip_addresses;
+        if (!dns_lookup(host_name, ip_addresses)) {
+            std::cout << "Failed to get ip from hostname" << std::endl;
+            return false;
+        }
 
-        path = slash_pos_1 == std::string::npos ? "" : url.substr(slash_pos_1);
+        std::stringstream request;
+        request << "GET " << path << " HTTP/1.1\r\n";
+        request << "Host: " << host_name << "\r\n\r\n";
 
-        host_name = std::string( url.begin() + offset, slash_pos_1 != std::string::npos ? url.begin() + slash_pos_1 : url.end() );
+        for (auto &ip_address: ip_addresses) {
+            try {
+                if (http_get(request.str(), ip_address, port, filename)) {
+                    std::cout << "File " << filename << " has been successfully loaded" << std::endl;
+                    return true;
+                }
+            } catch (std::exception &ex) {
+                std::cout << "Failed to perform http get for ip " << ip_address << ": " << ex.what() << std::endl;
+            }
+        }
 
-        path = (pos2 = path.find('#')) != std::string::npos ? path.substr(0, pos2) : path;
+        std::cout << "File " << filename << " was not loaded..." << std::endl;
+        return false;
+    }
 
-        url_port = (pos3 = host_name.find(':')) != std::string::npos ? host_name.substr(pos3 + 1) : "";
+private:
 
-        host_name = host_name.substr(0, pos3 != std::string::npos ? pos3 : host_name.length());
+    bool url_parse(const std::string &url, std::string &host_name, std::string &path, int &port) {
 
-        protocol = offset > 2 ? url.substr(0, offset - 3) : "";
+        size_t protocol_offset = url.find("://");
+        if (protocol_offset != std::string::npos) {
+            auto protocol = url.substr(0, protocol_offset);
+            if (protocol != ProtocolHttp) {
+                std::cout << "Protocol " << protocol << " is not supported..." << std::endl;
+                return false;
+            }
+            protocol_offset += 3;
+        }
 
-        query = (pos4 = path.find('?')) != std::string::npos ? path.substr(pos4+1) : "";
+        size_t slash_pos = url.find_first_of('/', protocol_offset);
+        path = slash_pos == std::string::npos ? "" : url.substr(slash_pos);
+        auto host_name_end = slash_pos != std::string::npos ? url.begin() + slash_pos : url.end();
+        host_name = std::string(url.begin() + protocol_offset, host_name_end);
 
-        path = pos4 != std::string::npos ? path.substr(0,pos4) : path;
+        size_t fragment_pos = path.find('#');
+        if (fragment_pos != std::string::npos) {
+            path = path.substr(0, fragment_pos);
+        }
+
+        size_t port_pos = host_name.find(':');
+        std::string url_port = port_pos != std::string::npos ? host_name.substr(port_pos + 1) : "80";
+        if (port_pos != std::string::npos) {
+            host_name = host_name.substr(0, port_pos);
+        }
+
+        size_t query_pos = path.find('?');
+        std::string query = query_pos != std::string::npos ? path.substr(query_pos + 1) : "";
+        if (query_pos != std::string::npos) {
+            path = path.substr(0, query_pos);
+        }
 
         if (query.length() > 0)
         {
-            path.reserve( path.length() + 1 + query.length() );
+            path.reserve(path.length() + 1 + query.length());
             path.append("?").append(query);
-        }
-
-        if (url_port.length()==0 && protocol.length()>0)
-        {
-            url_port = protocol=="http" ? "80" : "443";
         }
 
         if (host_name.empty()) {
@@ -75,41 +113,15 @@ public:
             return false;
         }
 
-        if (is_ipv6_address(host_name)) {
-            std::cout << "IPv6 not supported" << std::endl;
+        port = 0;
+        std::stringstream(url_port) >> port;
+        if (port == 0) {
+            std::cout << "Failed to parse url: invalid port: " << url_port << std::endl;
             return false;
         }
 
-        if (is_ipv4_address(host_name)) {
-            ip_addresses.emplace_back(host_name);
-        } else {
-            ip_addresses = dns_lookup(host_name);
-        }
-
-        if (ip_addresses.empty()) {
-            std::cout << "Failed to parse url: hostname not found";
-            return false;
-        }
-
-        std::stringstream(url_port) >> port; //string to int
-        std::stringstream request;
-        request << "GET " << path << " HTTP/1.1\r\n";
-        request << "Host: " << host_name << "\r\n\r\n";
-
-        for(int i = 0; i < ip_addresses.size(); ++i) {
-            try {
-                if (http_get(request.str(), ip_addresses[i], port, filename)) {
-                    return true;
-                }
-            } catch (std::exception &ex) {
-                std::cout << "Failed to perform http get: " << ex.what() << std::endl;
-            }
-        }
-
-        return false;
+        return true;
     }
-
-private:
 
     bool is_ipv6_address(const std::string& str)
     {
@@ -123,67 +135,80 @@ private:
         return inet_pton(AF_INET, str.c_str(), &(sa.sin_addr)) != 0;
     }
 
-    //----------------------------------------------------------------------
-    std::vector<std::string> dns_lookup(const std::string &host_name)
+    bool dns_lookup(const std::string &host_name, std::vector<std::string> &ip_addresses)
     {
-        std::vector<std::string> output;
+        if (is_ipv6_address(host_name)) {
+            std::cout << "IPv6 addresses are not supported" << std::endl;
+            return false;
+        }
+
+        if (is_ipv4_address(host_name)) {
+            ip_addresses.emplace_back(host_name);
+            return true;
+        }
 
         addrinfo hints{};
-        addrinfo *res = nullptr;
-        addrinfo *p = nullptr;
-
-        int status, ai_family;
         char ip_address[INET6_ADDRSTRLEN];
 
-        memset(&hints, 0, sizeof hints);
         hints.ai_family = AF_INET;
         hints.ai_socktype = SOCK_STREAM;
 
-        if ((status = getaddrinfo(host_name.c_str(), NULL, &hints, &res)) != 0) {
-            //cerr << "getaddrinfo: "<< gai_strerror(status) << endl;
-            return output;
+        addrinfo *res = nullptr;
+        if (getaddrinfo(host_name.c_str(), NULL, &hints, &res) != 0) {
+            std::cout << "Failed to get ip address of host " << host_name << std::endl;
+            return false;
         }
 
-        //cout << "DNS Lookup: " << host_name << " ipv:" << ipv << endl;
-
-        for(p = res;p != nullptr; p = p->ai_next) {
-            void *addr;
+        for(addrinfo *p = res; p != nullptr; p = p->ai_next) {
             auto ip = reinterpret_cast<struct sockaddr_in *>(p->ai_addr);
-            addr = &(ip->sin_addr);
+            void *address = &(ip->sin_addr);
 
-            // convert the IP to a string
-            inet_ntop(p->ai_family, addr, ip_address, sizeof(ip_address));
-            output.emplace_back(ip_address);
+            // convert IP to a string
+            inet_ntop(p->ai_family, address, ip_address, sizeof(ip_address));
+            ip_addresses.emplace_back(ip_address);
         }
 
-        freeaddrinfo(res); // free the linked list
-
-        return output;
+        freeaddrinfo(res);
+        return true;
     }
 
     int socket_connect(const std::string &ip_address, int port)
     {
-        int sd = -1;
         struct sockaddr_in sa{};
 
-        memset (&sa, '\0', sizeof(sa));
         sa.sin_family      = AF_INET;
         sa.sin_addr.s_addr = inet_addr(ip_address.c_str());   /* Server IP */
         sa.sin_port        = htons     (static_cast<uint16_t>(port));   /* Server Port number */
 
-        sd = ::socket(AF_INET, SOCK_STREAM, 0);
-        if (sd > 0 && ::connect(sd, (struct sockaddr*)&sa, sizeof(sa)) == 0) {
-            return sd;
+        int sd = ::socket(AF_INET, SOCK_STREAM, 0);
+        if (sd > 0) {
+            if (::connect(sd, (struct sockaddr*)&sa, sizeof(sa)) == 0) {
+                return sd;
+            }
+
+            ::close(sd);
         }
 
-        std::cout << "Socket connect error" << std::endl;
+        std::cout << "Socket connection error" << std::endl;
         return -1;
 
     }
 
-    std::string header_value(const std::string &full_header, const std::string& header_name)
-    {
-        size_t pos = full_header.find(header_name); //case sensitive but probably shouldn't be
+    std::string file_size_readable(int file_size_bytes) {
+        std::stringstream ss;
+        if (file_size_bytes < Bytes2pow10) {
+            ss << file_size_bytes << " b";
+        } else if (file_size_bytes < Bytes2pow10 * Bytes2pow10) {
+            ss << file_size_bytes / Bytes2pow10 << " K";
+        } else {
+            ss << file_size_bytes / Bytes2pow10 / Bytes2pow10 << " M";
+        }
+
+        return ss.str();
+    }
+
+    std::string header_value(const std::string &full_header, const std::string& header_name) {
+        size_t pos = full_header.find(header_name);
         std::string r;
         if (pos != std::string::npos)
         {
@@ -200,10 +225,6 @@ private:
 
     bool http_get(const std::string& request, const std::string& ip_address, int port, const std::string& fname)
     {
-        std::stringstream header;
-        std::string body_delimeter("\r\n\r\n");
-        auto body_delimiter_len = static_cast<int>(body_delimeter.length());
-
 
         std::ofstream fd(fname.c_str());
         if (!fd.good()) {
@@ -230,7 +251,9 @@ private:
         int delim_position = 0;
         int delim_end_ind = 0;
         ssize_t bytes_received = 0;
+        std::stringstream header;
 
+        // download header
         while (delim_not_found) {
             bytes_received = ::recv(sd, buffer.data(), buffer.size(), 0);
             if (bytes_received <= 0) {
@@ -241,8 +264,8 @@ private:
 
             for (int i = 0; i < bytes_received; ++i) {
                 header << buffer[i];
-                delim_position = buffer[i] == body_delimeter[delim_position] ? ++delim_position : 0;
-                if (delim_position >= body_delimiter_len) {
+                delim_position = buffer[i] == BodyDelimeter[delim_position] ? ++delim_position : 0;
+                if (delim_position >= BodyDelimiterLength) {
                     delim_not_found = false;
                     delim_end_ind = i + 1;
                     break;
@@ -250,9 +273,14 @@ private:
             }
         }
 
+        // download file
         int bytes_expected = 0;
         std::string h = header.str();
         std::stringstream(header_value(h, "Content-Length")) >> bytes_expected;
+        std::cout << "File size: " << file_size_readable(bytes_expected) << std::endl;
+
+        std::cout.setf(std::ios_base::unitbuf);
+        std::cout << std::setprecision(2) << std::fixed;
 
         int bytes_written = bytes_received - delim_end_ind;
         fd.write(buffer.data() + delim_end_ind, bytes_written);
@@ -262,8 +290,6 @@ private:
             if (bytes_received <= 0) {
                 std::cout << "Failed to read data from socket" << std::endl;
                 ::close(sd);
-                perror("Fail");
-                std::cout << "Errno: " << errno << std::endl;
                 return false;
             }
 
@@ -272,26 +298,26 @@ private:
             }
             bytes_written += bytes_received;
 
+            std::cout << "\rProgress: " << 100.0 * bytes_written / bytes_expected << "%";
             fd.write(buffer.data(), bytes_received);
         }
 
+        std::cout << std::endl;
         ::close(sd);
         return true;
     }
-
 
 };
 
 
 int main(int argc, char* argv[])
 {
-//    if (argc < 3) {
-//        cout << "Invalid arguments. Usage: downloader_binary url filename" << endl;
-//    }
-//     download(argv[1], argv[2]);
+    if (argc < 3) {
+        std::cout << "Invalid arguments. Usage: file_downloader url filename" << std::endl;
+    }
+
     auto downloader = HttpFileDownloader();
-//    downloader.file_download("http://www.cplusplus.com/ico/social_googleplus.png", "img.png");
-    downloader.file_download("http://manager.strij.lan:8787/media/%D0%A1%D1%82%D1%80%D0%B8%D0%B6.exe", "strij.exe");
+    downloader.file_download(argv[1], argv[2]);
 
     return 0;
 }
